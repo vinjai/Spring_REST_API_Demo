e70129b8d3bcca07db4df3444659298ec3a0101b
Vinayak Jaiswal <vinayak.j@media.net>
2018-02-25 21:01:29 +0530

    Unit TEST Integration

--- src/main/java/bookmarks/UserNotFoundException.java
+++ src/main/java/bookmarks/UserNotFoundException.java
@@ -1,8 +1,12 @@
 package bookmarks;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by vinayak.j on 2/24/2018.
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class UserNotFoundException extends RuntimeException {
     UserNotFoundException(String userId) {
         super("could not find user '" + userId + "'." );
